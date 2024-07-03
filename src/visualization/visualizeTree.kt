a45cf356b18b2ffe2a7a3b2f7edef6d12ff479f6
Nicolò Bertocco <nick.bertocco@gmail.com>
2024-07-03 23:54:50 +0200

    fixed duplicate frame

--- src/visualization/visualizeTree.kt
+++ src/visualization/visualizeTree.kt
@@ -4,7 +4,9 @@ import heaps.strict_fibonacci_heap.StrictFibonacciHeap
 import java.awt.Dimension
 import javax.swing.JFrame
 import org.graphstream.graph.implementations.SingleGraph
+import org.graphstream.ui.swing_viewer.SwingViewer
 import org.graphstream.ui.swing_viewer.ViewPanel
+import org.graphstream.ui.view.Viewer
 
 fun <T : Comparable<T>> visualizeTree(heap: StrictFibonacciHeap<T>) {
     System.setProperty("org.graphstream.ui", "swing")
@@ -13,17 +15,11 @@ fun <T : Comparable<T>> visualizeTree(heap: StrictFibonacciHeap<T>) {
     graph.setAttribute("ui.quality")
     graph.setAttribute("ui.stylesheet", "node {size: 60px, 60px;}")
 
-    val viewer = graph.display()
+    val viewer = SwingViewer(graph, Viewer.ThreadingModel.GRAPH_IN_GUI_THREAD)
     viewer.disableAutoLayout()
 
-    val viewPanel = viewer.defaultView as ViewPanel
+    val viewPanel = viewer.addDefaultView(false) as ViewPanel
     viewPanel.enableMouseOptions()
-
-    val frame = JFrame("GraphStream Viewer")
-    frame.contentPane.add(viewPanel)
-    frame.size = Dimension(1920, 1080)
-    frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
-    frame.isVisible = true
 
     val alreadyAddedNodes = mutableSetOf<String>()
     val alreadyAddedEdges = mutableSetOf<Pair<String, String>>()
@@ -32,4 +28,10 @@ fun <T : Comparable<T>> visualizeTree(heap: StrictFibonacciHeap<T>) {
     val t = TreeLayout(heap.heapRecord.root!!.item.toString())
     t.init(graph)
     t.compute()
+
+    val frame = JFrame("GraphStream Viewer")
+    frame.contentPane.add(viewPanel)
+    frame.size = Dimension(1920, 1080)
+    frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+    frame.isVisible = true
 }
